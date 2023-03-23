91361945426602e323b731c56bb2856b6a8a1c51
Sanoj C Sam <sanojcsam123@gmail.com>
2023-03-23 17:29:51 +0530

    FIRST COMMIT

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ArticlesModule } from './articles/articles.module';
+  import { StaticpagesModule } from './staticpages/staticpages.module';
+ import { RouterModule, Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    PagenotfoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    ArticlesModule,
+    StaticpagesModule,
+    AppRoutingModule,
+    
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
